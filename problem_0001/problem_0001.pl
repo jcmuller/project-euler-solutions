9dce6dedba46d976421102ec1e89746407aebd73
Juan C. Muller <jcmuller@gmail.com>
2010-12-29 12:07:34 -0500

    Solved problem 1.

---
+++ problem_0001/problem_0001.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl 
+use common::sense;
+
+sub is_multiple
+{
+	my ($n) = @_;
+
+	return 1 if ($n % 3 == 0);
+	return 1 if ($n % 5 == 0);
+	return 0;
+}
+
+my $sum = 0;
+for (1..999)
+{
+	$sum += $_ if (is_multiple($_));
+}
+
+print "$sum\n";
