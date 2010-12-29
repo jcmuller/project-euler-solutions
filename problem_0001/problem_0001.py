84088d22878631150a284c796305fbcd473dc910
Stefan Novak <stefan.louis.novak@gmail.com>
2010-12-29 13:30:20 -0500

    simplify problem 1

--- problem_0001/problem_0001.py
+++ problem_0001/problem_0001.py
@@ -1,9 +1,4 @@
-def solve():
-  s = 0
-  for n in range(1000):
-   if n%5==0 or n%3==0:
-     s += n
-  print s 
-
-if __name__ == '__main__':
-  solve()
+s = 0
+for n in range(1000):
+   s += n if n%5==0 or n%3==0 else 0
+print s
