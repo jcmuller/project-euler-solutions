9e0e6c13d65e73bb6103fd9dce38737f611e3a93
Juan C. Muller <jcmuller@gmail.com>
2010-12-29 12:07:49 -0500

    Solved problem 2.

---
+++ problem_0002/problem_0002.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl 
+use common::sense;
+
+sub fibo
+{
+	my ($num) = @_;
+	my $root = sqrt(5);
+
+	return int ( ( 1/$root * ( ( ( ( 1 + $root ) / 2 ) ** $num ) - ( ( ( 1 - $root ) / 2 ) ** $num ) ) ) );
+}
+
+my $max = 4 * 10**6;
+my $fib;
+my $total = 0;
+my $i = 0;
+
+while ($fib < $max)
+{
+	$i++;
+	$fib = fibo($i);
+	last if ($fib >= $max);
+
+	if ($fib % 2 == 0)
+	{
+		$total += $fib;
+		printf("fib(%2d) = %d\n", $i, $fib);
+	}
+}
+
+print "Total: $total\n";
+
