655f276fd6e6f625b970548dc8a7b9ef6775158c
Juan C. Muller <jcmuller@gmail.com>
2010-12-29 12:08:16 -0500

    Solved problem 6.

---
+++ problem_0006/problem_0006.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl 
+use common::sense;
+
+sub sum
+{
+	my ($func, $n, $max) = @_;
+
+	my $sum = 0;
+	for ($n..$max)
+	{
+		$sum += $func->($_);
+	}
+	return $sum;
+}
+
+sub sum_numbers
+{
+	my ($n) = @_;
+	return ($n * ($n + 1)) / 2;
+}
+
+my $limit = 100;
+my $n_sq = sub {my $n = shift; return $n ** 2};
+my $squares = sum($n_sq, 1, $limit);
+my $sum_square = sum_numbers($limit) ** 2;
+
+print $sum_square - $squares, "\n";
