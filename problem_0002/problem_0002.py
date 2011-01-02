6cf6b590b8781d5d86992a2ed0ad9ce2fef961f9
Stefan Novak <stefan.louis.novak@gmail.com>
2011-01-02 02:33:01 -0500

    refactored p2.py to use generator comprehension to increase performance.

--- problem_0002/problem_0002.py
+++ problem_0002/problem_0002.py
@@ -1,17 +1,9 @@
-def F():
-  F=[]
-  while True:
-    if not F:
-      F.append(1)
-      yield F[0]
-      F.append(2)
-    yield F[1]
-    F.append(sum(F))
-    F.pop(0)
-f = F()
-S = 0
-while True:
-  v = f.next()
-  if v > 4*10**6: break
-  if v%2==0: S+=v
-print S 
+def F(limit=float('Inf')):
+  a=0
+  b=1
+  while a+b<limit:
+   if (a+b)%2==0: yield a+b
+   a,b = b,a+b
+
+f = F(4*10**6)
+print sum(f)
