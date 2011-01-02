d73c7bd0525de5f27b0ff17b823fe4f198e0e8e4
Stefan Novak <stefan.louis.novak@gmail.com>
2011-01-01 23:13:16 -0500

    solved p1 using java

---
+++ problem_0001/problem_0001.java
@@ -0,0 +1,10 @@
+class problem_0001
+{
+  public static void main(String args[]){
+    int sum = 0;
+    for (int n=0; n<1000; n++){
+      if (n%3==0 || n%5==0) sum+=n;
+    }
+    System.out.println(sum);
+  }
+}
