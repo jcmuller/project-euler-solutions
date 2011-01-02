d6a79d59bea357b6390775311a08ae868eab0202
Stefan Novak <stefan.louis.novak@gmail.com>
2011-01-01 20:55:00 -0500

    increased performance of p4.py

--- problem_0004/problem_0004.py
+++ problem_0004/problem_0004.py
@@ -1,7 +1,10 @@
-sol = []
-for i in range(100,999):
-  for j in range(i,999):
+from sys import exit
+sol =0 
+for i in range(999,100,-1):
+  for j in range(999,i,-1):
+    if i*j<sol: break
     p = '%i'%(i*j)
     if p==p[::-1]:
-      sol.append(i*j)
-print sorted(sol)[-1]
+      sol = max(sol,i*j)
+      break
+print sol
