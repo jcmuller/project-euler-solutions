115156d0e039d4a25607e2741fcb7b8bfbac0cdc
Stefan Novak <stefan.louis.novak@gmail.com>
2011-01-04 19:35:22 -0500

    solved p55 using python

--- problem_0055/problem_0055.py
+++ problem_0055/problem_0055.py
@@ -1,10 +1,8 @@
 def is_lychrel(N):
-  if N>10 and str([N])[-1]=='0': return False
+  if str([N])[-1]=='0': return False
   for i in range(50):
-    print N
-    print int(str(N)[::-1])
-    N = N + int(str(N)[::-1])
-    if str(N)==str(N)[::-1]:  return True
-  return False
+    N += int(str(N)[::-1])
+    if str(N)==str(N)[::-1]:  return False
+  return True
 
-print sum([True for n in range(,11) if is_lychrel(n)])
+print sum([True for n in range(10**4) if is_lychrel(n)])
