4ca58ec34d3e3e845c8ba501cc54438978de62b2
Stefan Novak <stefan.louis.novak@gmail.com>
2011-01-06 22:31:24 -0500

    update p3.py to use factor method in resources

--- problem_0003/problem_0003.py
+++ problem_0003/problem_0003.py
@@ -1,17 +1,6 @@
 import sys
-N = 600851475143
-prime_factors = []
-while True:
-  abort = True
-  i = 2
-  while i<N:
-    if N%i==0:
-      prime_factors.append(i)
-      N=N/i
-      abort = False
-      break
-    i+=1
-  if abort:
-    prime_factors.append(N)
-    print prime_factors[-1]
-    sys.exit()
+sys.path.append('../_resources')
+
+from resources import factor
+
+print max(factor(600851475143)) 
